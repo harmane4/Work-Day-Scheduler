fef81552226cd65dbbb2e1ab85d0b57493ea5105
Elise Harman <elise_harman@hotmail.com>
2021-04-07 09:09:49 +0800

    Added if statements for 9am timeblock to include the current and past times to change colour to red and green

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+//Display current date at the top of the calendar
+$("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+console.log(moment().format("dddd, MMM Do YYYY"));
 
-  //Display current date at the top of the calendar
-  $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
-  console.log(moment().format("dddd, MMM Do YYYY"));
-
-//Specify a function to execute when DOM if sully loaded
+//Specify a function to execute when DOM if fully loaded
 $(document).ready(function () {
-
   //SaveBtn with on click function
   $(".saveBtn").on("click", function () {
     let value = $(this).siblings(".description").val();
@@ -14,7 +12,7 @@ $(document).ready(function () {
     console.log(time);
   });
 
-    //Retrieves schedule items from local storage
+  //Retrieves schedule items from local storage
   retrieveSchedule();
 
   function retrieveSchedule() {
@@ -26,85 +24,107 @@ $(document).ready(function () {
     if (currentTime > 9) {
       $("#firstTimeBlock").css({ "background-color": "#ccccc7" });
     }
+    if (currentTime == 9) {
+      $("#firstTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 9) {
+      $("#firstTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var secondTime = localStorage.getItem("10:00");
     $("#secondTimeBlock").val(secondTime);
     if (currentTime > 10) {
       $("#secondTimeBlock").css({ "background-color": "#ccccc7" });
-    } if (currentTime == 10) {
-        $("#secondTimeBlock").css({ "background-color": "#DC143C" });
-      } if (currentTime < 10) {
-        $("#secondTimeBlock").css({ "background-color": "#006400" });
-      }
+    }
+    if (currentTime == 10) {
+      $("#secondTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 10) {
+      $("#secondTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var thirdTime = localStorage.getItem("11:00");
     $("#thirdTimeBlock").val(thirdTime);
-      if (currentTime > 11) {
-        $("#thirdTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 11) {
-          $("#thirdTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 11) {
-          $("#thirdTimeBlock").css({ "background-color": "#006400" });
-        }
+    if (currentTime > 11) {
+      $("#thirdTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 11) {
+      $("#thirdTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 11) {
+      $("#thirdTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var fourthTime = localStorage.getItem("12:00");
     $("#fourthTimeBlock").val(fourthTime);
-  if (currentTime > 12) {
-        $("#fourthTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 12) {
-          $("#fourthTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 12) {
-          $("#fourthTimeBlock").css({ "background-color": "#006400" });
-        }
+    if (currentTime > 12) {
+      $("#fourthTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 12) {
+      $("#fourthTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 12) {
+      $("#fourthTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var fifthTime = localStorage.getItem("13:00");
     $("#fifthTimeBlock").val(fifthTime);
     if (currentTime > 13) {
-        $("#fifthTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 13) {
-          $("#fifthTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 13) {
-          $("#fifthTimeBlock").css({ "background-color": "#006400" });
-        }
+      $("#fifthTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 13) {
+      $("#fifthTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 13) {
+      $("#fifthTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var sixthTime = localStorage.getItem("14:00");
     $("#sixthTimeBlock").val(sixthTime);
     if (currentTime > 14) {
-        $("#sixthTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 14) {
-          $("#sixthTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 14) {
-          $("#sixthTimeBlock").css({ "background-color": "#006400" });
-        }
+      $("#sixthTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 14) {
+      $("#sixthTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 14) {
+      $("#sixthTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var seventhTime = localStorage.getItem("15:00");
     $("#seventhTimeBlock").val(seventhTime);
     if (currentTime > 15) {
-        $("#seventhTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 15) {
-          $("#seventhTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 15) {
-          $("#seventhTimeBlock").css({ "background-color": "#006400" });
-        }
+      $("#seventhTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 15) {
+      $("#seventhTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 15) {
+      $("#seventhTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var eighthTime = localStorage.getItem("16:00");
     $("#eighthTimeBlock").val(eighthTime);
     if (currentTime > 16) {
-        $("#eighthTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 16) {
-          $("#eighthTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 16) {
-          $("#eighthTimeBlock").css({ "background-color": "#006400" });
-        }
+      $("#eighthTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 16) {
+      $("#eighthTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 16) {
+      $("#eighthTimeBlock").css({ "background-color": "#006400" });
+    }
 
     var ninthTime = localStorage.getItem("17:00");
     $("#ninthTimeBlock").val(ninthTime);
     if (currentTime > 17) {
-        $("#ninthTimeBlock").css({ "background-color": "#ccccc7" });
-      } if (currentTime == 17) {
-          $("#ninthTimeBlock").css({ "background-color": "#DC143C" });
-        } if (currentTime < 17) {
-          $("#ninthTimeBlock").css({ "background-color": "#006400" });
-        }
+      $("#ninthTimeBlock").css({ "background-color": "#ccccc7" });
+    }
+    if (currentTime == 17) {
+      $("#ninthTimeBlock").css({ "background-color": "#DC143C" });
+    }
+    if (currentTime < 17) {
+      $("#ninthTimeBlock").css({ "background-color": "#006400" });
+    }
   }
 });
